a9ff83e08cf28a041caa64ca6b5e7729230a4796
Neojhonn <jhonny.azevedo@gmail.com>
2023-08-09 12:01:45 -0300

    first commmit

---
+++ Data/MovieMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MoviesMVC.Models;
+
+namespace MoviesMVC.Data
+{
+    public class MovieMVCContext : DbContext
+    {
+        public MovieMVCContext (DbContextOptions<MovieMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MoviesMVC.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace MoviesMVC.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Título do Filme:")]
+        [Required(ErrorMessage = "Digite o título do Filme")]
+        public string? Title { get; set; }
+
+        [DisplayName("Gênero do Filme:")]
+        [Required(ErrorMessage = "Digite o gênero do Filme")]
+        public string? Genre { get; set; }
+
+        [DisplayName("Data de Lançamento:")]
+        [Required(ErrorMessage = "Insira a data de lançamento")]
+        [DataType(DataType.Date)]
+        public DateTime RealeseDate { get; set; }
+
+    }
+}
